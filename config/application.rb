b60e8faeadb632d9fa9892079c3190a0f1e883ee
yamakami21 <ya_makami@hotmail.co.jp>
2015-11-14 05:37:11 +0000

    added time

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
 module CollectionList
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
