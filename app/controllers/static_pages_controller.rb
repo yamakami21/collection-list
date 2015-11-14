04354c8f35ced68953d941a187f170d6bae7bc7b
yamakami21 <ya_makami@hotmail.co.jp>
2015-11-14 03:42:37 +0000

    desinged user page and added edit user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @item = current_user.items.build
       @feed_items = current_user.feed_items.includes(:user).page(params[:page]).per(10).order(created_at: :desc)
+    else
+      redirect_to login_url
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :followings, :followers, :likings]
+  before_action :set_user, only: [:show, :edit, :update, :followings, :followers, :likings]
   
   def show
     @items =@user.items.page(params[:page]).per(10)
@@ -17,6 +17,19 @@ class UsersController < ApplicationController
       redirect_to @user # ここを修正
     else
       render 'new'
+    end
+  end
+  
+  def edit
+
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] =  "更新しました！"
+      redirect_to user_path
+    else
+      render 'edit'
     end
   end
   
